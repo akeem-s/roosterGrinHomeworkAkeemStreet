0de299d8fe1292c4615237796dd40cfd7e13abce
akeem-s <akeemstreet@gmail.com>
2017-03-18 15:23:57 -0700

    Add user model and migration. Seed file parsing csv file as expected

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,10 @@
 
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'real_estate_transactions.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'patients.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   user = User.new
-  user.id = row['id']
   user.first_name = row['first_name']
   user.last_name = row['last_name']
   user.phone_number = row['phone_number']
