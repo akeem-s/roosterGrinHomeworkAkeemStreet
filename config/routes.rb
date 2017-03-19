c4370389542b050169ac5d038a32ad4485bb287b
akeem-s <akeemstreet@gmail.com>
2017-03-18 18:28:25 -0700

    Add basic form layout and Materialize.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/notes/new', to: 'notes#new'
+  root 'notes#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
